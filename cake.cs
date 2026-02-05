d6ed09c77b69fc7b15c95f4da49d73f384e3e1cf
Dominik Stahmer <dstahmer@recom.eu>
2026-02-05 13:11:10 +0100

    cake config

---
+++ cake.cs
@@ -0,0 +1,73 @@
+#:sdk Cake.Sdk@6.0.0
+
+var target = Argument("target", "build");
+var configuration = Argument("configuration", "Release");
+
+// ----------------------------------------------------------------------
+// Tasks
+// ----------------------------------------------------------------------
+
+Task("build")
+    .Does(() =>
+    {
+        DotNetBuild("./src/Dmnk.Toolkit.slnx", new DotNetBuildSettings
+        {
+            Configuration = configuration,
+        });
+    });
+
+Task("test")
+    .IsDependentOn("Build")
+    .Does(() =>
+    {
+        DotNetTest("./src/Dmnk.Toolkit.slnx", new DotNetTestSettings
+        {
+            Configuration = configuration,
+            NoBuild = true,
+        });
+    });
+
+Task("docs:clean")
+    .Does(() =>
+    {
+        RimRaf("./docs/api");
+        RimRaf("./docs/_site");
+    });
+
+Task("docs:build")
+    .IsDependentOn("build")
+    .IsDependentOn("docs:clean")
+    .Does(() =>
+    {
+        StartProcess("dotnet", new ProcessSettings {
+            Arguments = "docfx docs/docfx.json"
+        });
+    });
+
+Task("docs:serve")
+    .IsDependentOn("docs:build")
+    .Does(() =>
+    {
+        StartProcess("dotnet", new ProcessSettings {
+            Arguments = "docfx serve",
+            WorkingDirectory = "docs/_site"
+        });
+    });
+
+// ----------------------------------------------------------------------
+// Helpers
+// ----------------------------------------------------------------------
+
+void RimRaf(string path) {
+    var rimraf = new DeleteDirectorySettings {
+        Recursive = true,
+        Force = true
+    };
+    if (System.IO.Path.Exists(path)) DeleteDirectory(path, rimraf);
+}
+
+// ----------------------------------------------------------------------
+// Execution
+// ----------------------------------------------------------------------
+
+RunTarget(target); 
